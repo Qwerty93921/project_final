6a68e7c87a735d73b9f6887a887c9bc5ef364830
Qwerty93921 <134071623+Qwerty93921@users.noreply.github.com>
2024-06-18 20:24:14 +0500

    final_project_1_file

---
+++ final_project/final_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html', context={})
+
+# def about(request):
+#     return render(request, 'about.html', context={})
+#
+#
+# def home(request):
+#     return render(request, 'home.html', context={})
